8b1e0737a3cde3d5519060cce97f6ab4abdb9445
Michelle Clement <michellelclement@gmail.com>
2022-03-08 13:47:46 +0000

    Make play icons buttons and remove button styling

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const app = () => {
+    
+    //Left Timer
+    const playLeft = document.querySelector(".play-left");
+    const timerLeft = document.querySelector(".time-display-left");
+    
+    //Right Timer
+    const playRight = document.querySelector(".play-right");
+    const timerRight = document.querySelector(".time-display-right");
+    
+    //Total Timer
+    
+    playLeft.addEventListener("click", () => {
+        
+    })
+
+}
+
+
+app();
